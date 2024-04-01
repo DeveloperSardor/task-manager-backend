c2ec3975a66e230a0b3ec0c627a20b509a6425ab
boyqoziyev610 <sboyqoziyev9@gmail.com>
2024-04-01 15:28:35 +0500

    inital commit

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 const server = http.createServer(app);
 
-app.use(cors());
+app.use(cors('*'));
 app.use('/uploads', express.static('uploads'));
 app.use(express.json());
 app.use('/api', api);
